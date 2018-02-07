e9be158353a4852162ebfd11424933494598eca5
ccaper <christian.caperton@performics.com>
2018-02-07 17:29:13 -0600

    completed course 18

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyAug5SaIvOMTM11RhE_IAC65s6Q9-DsPok",
+  authDomain: "catch-of-the-day-ccaper.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-ccaper.firebaseio.com"
+});
+
+export default base;
